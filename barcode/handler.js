e917a046cba29c93a3c443b0cfc17a51e7ec21bd
Patricio Diaz <padiazg@gmail.com>
2018-07-31 17:52:58 -0400

    Changed from rescode to bwip-js

--- barcode/handler.js
+++ barcode/handler.js
@@ -1,80 +1,52 @@
 "use strict";
-const rescode = require("rescode");
+const bwipjs = require("bwip-js");
 
 module.exports = (context, callback) => {
     // always receive params as a json object
-    let parms;
+    let options;
     try {
-        parms = JSON.parse(context);
+        options = JSON.parse(context);
     } catch (e) {
         callback(e, undefined);
         return;
     }
 
-    // code type requested
-    const code = parms.code.toLowerCase();
-
-    // value to display
-    const value = parms.value;
-
-    // scale
-    const scale = parms.scale || 0;
-
-    // output format
-    const format = parms.fmt ? parms.fmt.toLowerCase() : "png";
+    if (!options.bcid || !options.text) {
+        callback('bdid & text are required', undefined);
+        return;
+    } // if (!options.bcid || !options.text) ...
 
     // encode to base64
-    const base64 = parms.base64 ? parms.base64 : false;
+    const base64 = options.base64 ? options.base64 : false;
+    delete options.base64;
 
-    const render_options = {
-        includetext: true,
-        guardwhitespace: true,
-        inkspread: 0,
-        scaleX: scale,
-        scaleY: scale
-    };
+    // set some defaults
+    options.bcid = options.bcid.toLowerCase();
+    options.backgroundcolor = options.backgroundcolor ? options.backgroundcolor : 'FFFFFF';
+    options.padding = options.padding ? options.padding : 1;
+    options.guardwhitespace = true;
+    options.paddingwidth = options.paddingwidth != undefined ? options.paddingwidth : 1;
+    options.paddingheight = options.paddingheight != undefined ? options.paddingheight : 1;
 
-    const modules = [];
+    // const options = {
+    //     bcid,
+    //     text,
+    //     guardwhitespace: true,
+    //     inkspread: 0,
+    //     scale,
+    //     // showborder,
+    //     // bordercolor,
+    //     // borderwidth,
+    //     paddingwidth: padding,
+    //     paddingheight: padding,
+    //     backgroundcolor,
+    // };
 
-    switch (code) {
-        case "code128":
-            modules.push("code128");
-            break;
-
-        case "code39":
-            modules.push("code39");
-            break;
-
-        case "ean13":
-            modules.push("ean2", "ean5", "ean8", "ean13");
-            break;
-
-        case "interleaved2of5":
-            module.push("interleaved2of5");
-            break;
-
-        case "pdf417":
-            modules.push("pdf417");
-            render_options.includetext = false;
-            break;
-
-        case "qrcode":
-            modules.push("qrcode");
-            render_options.includetext = false;
-            break;
-
-        case "datamatrix":
-            modules.push("datamatrix");
-            render_options.includetext = false;
-            break;
-
-        default:
-            callback("Unknown code requested: " + code, undefined);
-            return;
-    } // switch (code) ...
-
-    rescode.loadModules(modules, render_options);
-    const bc = rescode.create(code, value);
-
-    callback(undefined, base64 ? bc.toString("base64") : bc);
+    bwipjs.toBuffer(options, (error, png) => {
+        if (error) {
+            callback(error, undefined);
+        } else {
+            callback(undefined, base64 ? png.toString("base64") : png);
+        }
+    })
 };
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 # OpenFaaS - Barcode generator
 
-Do you need to generate barcodes or qr codes? This function can generate ```EAN13```, ```QR```, ```PDF417```, ```CODE39```, ```CODE128```, ```DataMatrix``` and ```Interleaved 2 of 5```
+Do you need to generate barcodes or qr codes? This function can generate a lot of them.
 
-> This is a port of [barcode-as-a-service](https://github.com/padiazg/barcode-as-a-service). Also published on [Heroku](https://barcode-render.herokuapp.com/).
+>This project uses [bqip-js](https://github.com/metafloor/bwip-js).  
+bwip-js is a translation to native JavaScript of the amazing code provided in [Barcode Writer in Pure PostScript](https://github.com/bwipp/postscriptbarcode). The translated code can run on any modern browser or JavaScript-based server framework.
 
 ## Before you start
 
@@ -23,24 +24,39 @@ faas-cli build && faas-cli deploy
 ```
 
 ## Parameters:
-Parameters must be passed in JSON format:
+Parameters must be passed in JSON format:  
 Example:
 ```json
 {
-  "code": "qrcode", 
-  "value": "Hello world from OpenFaaS!"
+  "bcid": "qrcode",
+  "text": "Hello world from OpenFaaS!"
 }
 ```
+Specific from this project:
+* **base64**: Optional, not part of the modules parameters. Tells the function that we want the ouput encoded in base64. Defaults tu ```false```.
 
-* **code**: The requested code. *Mandatory*  
+Specific from bwip-js
+* **bcid**: The requested code. *required*  
     Posible values are
-    + ean13
-    + qr
-    + pdf417
-    + code39
-    + code128
-    + datamatrix
-    + interleaved2of5
-* **value**: The value to represent. *Mandatory*
-* **scale**: The scale of the image. Defaults to 0, increase the value to get a larger image. *Optional*
-* **base64**: If we want the ouput encoded in base64. Defaults tu false. *Optional*
+
+* **text**: The value to represent. *required*
+
+* **scaleX**: The x-axis scaling factor. Must be an integer > 0. Default is 2.
+
+* **scaleY**: The y-axis scaling factor. Must be an integer > 0. Default is scaleX.
+
+* **scale**: Sets both the x-axis and y-axis scaling factors. Must be an integer > 0.
+
+* **rotate**: Allows rotating the image to one of the four orthogonal orientations. A string value. Must be one of:
+
+    "N" : Normal (not rotated). The default.
+    "R" : Clockwise (right) 90 degree rotation.
+    "L" : Counter-clockwise (left) 90 degree rotation.
+    "I" : Inverted 180 degree rotation.
+    paddingwidth : Sets the left and right padding (in points/pixels) around the rendered barcode. Rotates and scales with the image.
+
+* **paddingheight**: Sets the top and bottom padding (in points/pixels) around the rendered barcode. Rotates and scales with the image.
+
+* **monochrome**: Sets the human-readable text to render in monochrome. Boolean true or false. Default is false which renders 256-level gray-scale anti-aliased text.
+
+There are more options specific from BWIPP, and they are documented in the  [BWIPP wiki](https://github.com/bwipp/postscriptbarcode/wiki). You need to consult there to determine what options are available for each barcode type.
